255f3cc1b5ea07a3ab199d45d92e470751606c1c
mittsmasa <light62motion@gmail.com>
2024-12-25 20:05:45 +0900

    fix: blue などはなくなったので置き換える

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import { css } from '@/styled-system/css';
 export default function Home() {
   return (
     <div>
-      <main className={css({ color: 'blue.500' })}>
+      <main
+        className={css({ color: 'yellow.800', textStyle: 'Heading.primary' })}
+      >
         <SignIn />
       </main>
     </div>
