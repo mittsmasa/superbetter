bc223fcb6a2997d3e22314913cd61f2ac5a2b3a4
mittsmasa <light62motion@gmail.com>
2024-12-25 20:24:29 +0900

    feat: setup PixelMPlus font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -7,13 +8,23 @@ export const metadata: Metadata = {
     'これは「スーパーベターになろう！」の書籍を実践しやすくするために丹精込めて作られたアプリケーションです',
 };
 
+const pixelMPlus = localFont({
+  src: [
+    {
+      path: '../fonts/PixelMplus12-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={pixelMPlus.className}>
       <body>{children}</body>
     </html>
   );
