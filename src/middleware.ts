b37f275235322135b21f12e48da6f0fa9dbf95c9
mittsmasa <light62motion@gmail.com>
2024-12-26 11:16:20 +0900

    feat: protect resource

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,17 @@
 import NextAuth from 'next-auth';
-import { config } from './auth/config';
+import { config as authConfig } from './auth/config';
 
-export const { auth: middleware } = NextAuth(config);
+const { auth } = NextAuth(authConfig);
+
+const PUBLIC_PATHS = ['/login'];
+
+export default auth((req) => {
+  if (!req.auth && !PUBLIC_PATHS.includes(req.nextUrl.pathname)) {
+    const newUrl = new URL('/login', req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
