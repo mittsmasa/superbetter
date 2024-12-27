ab45e4ea994b6ca33a73d5516cb4312b4aa4feaa
mittsmasa <light62motion@gmail.com>
2024-12-27 17:41:13 +0900

    fix: 一旦必要になったら足してくでいいや

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,1 +1,5 @@
+export { default as Network4G } from './pixel-icons/4g.svg';
+export { default as Resolution4KBox } from './pixel-icons/4k-box.svg';
+export { default as Resolution4K } from './pixel-icons/4k.svg';
+export { default as Network5G } from './pixel-icons/5g.svg';
 export { default as AbTesting } from './pixel-icons/ab-testing.svg';
