39b7740ee7e1ecfe85431698f51ffb09ab693491
mittsmasa <light62motion@gmail.com>
2025-01-01 16:34:00 +0900

    feat: mission entities を追加

--- src/app/_components/mission/index.tsx
+++ src/app/_components/mission/index.tsx
@@ -1,16 +1,17 @@
-import { Android, ScriptText, Zap } from '@/assets/icons';
 import { PixelBorder } from '@/components/pixel-border';
 import { css } from '@/styled-system/css';
 import Link from 'next/link';
-import type { MissionItem as MissionItemType } from '../../../../types/mission';
+import { MissionEntities, type MissionEntity } from './entitity';
 
-type MissionProps = {
+export const Mission = ({
+  id,
+  title,
+  items,
+}: {
   id: string;
   title: string;
-  items: MissionItemProps[];
-};
-
-export const Mission = ({ id, title, items }: MissionProps) => {
+  items: MissionEntity[];
+}) => {
   return (
     <PixelBorder width="[100%]">
       <Link
@@ -25,75 +26,8 @@ export const Mission = ({ id, title, items }: MissionProps) => {
         })}
       >
         <p>{title}</p>
-        <MissionItemList items={items} />
+        <MissionEntities items={items} />
       </Link>
     </PixelBorder>
   );
-};
-
-const MissionItemList = (props: { items: MissionItemProps[] }) => {
-  return (
-    <div
-      className={css({
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '12px',
-        overflowX: 'auto',
-      })}
-    >
-      {props.items.map((item, index) => (
-        <div
-          key={index}
-          className={css({
-            height: '[24px]',
-            width: '[24px]',
-            flexShrink: 0,
-          })}
-        >
-          <MissionItem key={index} {...item} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-type MissionItemProps = {
-  id: string;
-  missionItemType: MissionItemType;
-  completed: boolean;
-};
-
-const MissionItem = ({ missionItemType, completed }: MissionItemProps) => {
-  switch (missionItemType) {
-    case 'quest':
-      return (
-        <ScriptText
-          className={css({
-            color: completed ? 'cyan.500' : 'gray.500',
-            transition: '[color 0.8s ease-in-out]',
-          })}
-        />
-      );
-    case 'powerup':
-      return (
-        <Zap
-          className={css({
-            color: completed ? 'yellow.300' : 'gray.500',
-            transition: '[color 0.8s ease-in-out]',
-          })}
-        />
-      );
-    case 'villain':
-      return (
-        <Android
-          className={css({
-            color: completed ? 'purple.600' : 'gray.500',
-            transition: '[color 0.8s ease-in-out]',
-          })}
-        />
-      );
-    default:
-      return null;
-  }
 };
