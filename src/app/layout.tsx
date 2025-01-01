270259708366ac770ade52e58c40f8fa17c3363d
mittsmasa <light62motion@gmail.com>
2025-01-01 18:35:04 +0900

    refactor: デフォルトの色情報を globalCss に集約

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className={pixelMPlus.className}>
-      <body className={css({ backgroundColor: 'black', color: 'white' })}>
-        {children}
-      </body>
+      <body className={css({ padding: '8px 12px' })}>{children}</body>
     </html>
   );
 }
