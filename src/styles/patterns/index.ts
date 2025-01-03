ac16825ee2f9320a67f0468ea5efb1b1aec6d8b7
mittsmasa <light62motion@gmail.com>
2025-01-03 19:26:01 +0900

    feat: pixel border の pattern を定義

---
+++ src/styles/patterns/index.ts
@@ -0,0 +1,26 @@
+import { definePattern } from '@pandacss/dev';
+
+const pixelBorder = definePattern({
+  description: 'A container that has pixel borders',
+  defaultValues: {
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  properties: {
+    borderWidth: { type: 'number' },
+    borderColor: { type: 'property', value: 'color' },
+  },
+  strict: true,
+  transform(props) {
+    const { borderColor, borderWidth } = props;
+    return {
+      '--pixel-border-width': `${borderWidth}px`,
+      '--pixel-border-color': borderColor,
+      margin: 'var(--pixel-border-width)',
+      boxShadow:
+        'calc(-1 * var(--pixel-border-width)) 0 0 0 var(--pixel-border-color), var(--pixel-border-width) 0 0 0 var(--pixel-border-color), 0 calc(-1 * var(--pixel-border-width)) 0 0 var(--pixel-border-color), 0 var(--pixel-border-width) 0 0 var(--pixel-border-color)',
+    };
+  },
+});
+
+export { pixelBorder };
