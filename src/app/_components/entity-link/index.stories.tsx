0ba792992fff57df69e430505e86663b27117600
mittsmasa <light62motion@gmail.com>
2025-01-18 14:39:52 +0900

    feat: quests の画面を追加

---
+++ src/app/_components/entity-link/index.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { EntityLink } from '.';
+
+const meta = {
+  component: EntityLink,
+  args: {
+    href: '#',
+    title: 'たいとる',
+    description: 'せつめい',
+  },
+} satisfies Meta<typeof EntityLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {} satisfies Story;
