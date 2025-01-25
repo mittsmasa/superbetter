dac6efb5c7821e31d6a5a960c8615ef26f1cd9b8
mittsmasa <light62motion@gmail.com>
2025-01-25 10:50:12 +0900

    refactor: DeleteConfirmButtonのなかで DeleteConfirmButton つかうように

--- src/app/powerups/[id]/_components/delete-confirm-button/index.tsx
+++ src/app/powerups/[id]/_components/delete-confirm-button/index.tsx
@@ -1,8 +1,7 @@
 'use client';
 
+import { DeleteConfirmDialog } from '@/app/_components/delete-confirm-dialog';
 import { Trash } from '@/assets/icons';
-import { Button } from '@/components/button';
-import { Dialog } from '@/components/dialog';
 import { IconButton } from '@/components/icon-button';
 import { useDialog } from '@/hooks/dialog';
 import { css } from '@/styled-system/css';
@@ -14,48 +13,13 @@ export const DeleteConfirmButton = (props: { id: string; name: string }) => {
       <IconButton onClick={deleteConfirm.show}>
         <Trash className={css({ width: '[24px]', height: '[24px]' })} />
       </IconButton>
-      <Dialog ref={deleteConfirm.ref}>
-        <div
-          className={css({
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '24px',
-          })}
-        >
-          <div>
-            <p className={css({ textAlign: 'center', lineClamp: 3 })}>
-              {props.name}
-            </p>
-            <p
-              className={css({
-                textAlign: 'center',
-                textStyle: 'Body.secondary',
-              })}
-            >
-              を削除しますか
-            </p>
-          </div>
-          <form
-            action={async () => {}}
-            className={css({
-              display: 'flex',
-              gap: '8px',
-              justifyContent: 'center',
-            })}
-          >
-            <Button type="submit">
-              <div className={css({ width: '[100px]' })}>さくじょ</div>
-            </Button>
-            <Button
-              type="button"
-              onClick={deleteConfirm.close}
-              variant="secondary"
-            >
-              <div className={css({ width: '[100px]' })}>キャンセル</div>
-            </Button>
-          </form>
-        </div>
-      </Dialog>
+      <DeleteConfirmDialog
+        dialog={deleteConfirm}
+        itemName={props.name}
+        onDelete={() => {
+          deleteConfirm.close();
+        }}
+      />
     </>
   );
 };
