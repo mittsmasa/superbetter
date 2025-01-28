1c33aa3d09bbf4d6f540b91db057a3eb98b21b5b
mittsmasa <light62motion@gmail.com>
2025-01-28 20:21:03 +0900

    feat: quests を実装

---
+++ src/app/_actions/get-quest.ts
@@ -0,0 +1,60 @@
+import 'server-only';
+
+import { getUser } from '@/app/_actions/get-user';
+import type { Result } from '@/app/_actions/types/result';
+import { db } from '@/db/client';
+import { quests } from '@/db/schema/superbetter';
+import { eq } from 'drizzle-orm';
+
+export const getQuests = async (ops?: { limit: number }): Promise<
+  Result<
+    { records: (typeof quests.$inferSelect)[]; count: number },
+    { type: 'unknown'; message: string }
+  >
+> => {
+  const user = await getUser();
+  try {
+    const qs = await db.query.quests.findMany({
+      where: (quest) => eq(quest.userId, user.id),
+      limit: ops?.limit,
+    });
+    const qCount = await db.$count(quests);
+    return { type: 'ok', data: { records: qs, count: qCount } };
+  } catch (e) {
+    console.error(e);
+    return {
+      type: 'error',
+      error: { type: 'unknown', message: 'unknown error' },
+    };
+  }
+};
+
+export const getQuest = async (
+  questId: string,
+): Promise<
+  Result<
+    typeof quests.$inferSelect,
+    { type: 'not-found' | 'unknown'; message: string }
+  >
+> => {
+  const user = await getUser();
+  try {
+    const q = await db.query.quests.findFirst({
+      where: (quest) => eq(quest.id, questId),
+    });
+
+    if (!q) {
+      return {
+        type: 'error',
+        error: { type: 'not-found', message: 'specific id not found.' },
+      };
+    }
+    return { type: 'ok', data: q };
+  } catch (e) {
+    console.error(e);
+    return {
+      type: 'error',
+      error: { type: 'unknown', message: 'unknown error' },
+    };
+  }
+};
---
+++ src/app/quests/_actions/edit-quest.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { getUser } from '@/app/_actions/get-user';
+import type { Result } from '@/app/_actions/types/result';
+import { db } from '@/db/client';
+import { quests } from '@/db/schema/superbetter';
+import { and, eq } from 'drizzle-orm';
+import { revalidatePath } from 'next/cache';
+
+export const editQuest = async (args: {
+  questId: string;
+  name: string;
+  description?: string | null;
+}): Promise<Result<undefined, { type: 'unknown'; message: string }>> => {
+  const user = await getUser();
+  try {
+    await db
+      .update(quests)
+      .set({
+        title: args.name,
+        description: args.description,
+      })
+      .where(and(eq(quests.id, args.questId), eq(quests.userId, user.id)));
+    revalidatePath('/quests/[id]', 'page');
+  } catch (e) {
+    console.error(e);
+    return {
+      type: 'error',
+      error: { type: 'unknown', message: 'unknown error' },
+    };
+  }
+  return { type: 'ok', data: undefined };
+};
