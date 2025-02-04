198cc2e30029ed3e43f881037b0deac09f2a3261
mittsmasa <light62motion@gmail.com>
2025-02-04 21:29:46 +0900

    feat: vibrator provider を実装

---
+++ src/app/_providers/vibration/index.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { css } from '@/styled-system/css';
+import { type ReactNode, createContext, use, useCallback, useRef } from 'react';
+
+interface VibrationContextProps {
+  vibrate: () => void;
+}
+
+const VibrationContext = createContext<VibrationContextProps | undefined>(
+  undefined,
+);
+
+export const VibrationProvider = ({ children }: { children: ReactNode }) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const vibrate = useCallback(
+    () => window.navigator.vibrate?.([10]) ?? ref.current?.click(),
+    [],
+  );
+  return (
+    <VibrationContext.Provider value={{ vibrate }}>
+      {children}
+      <input
+        ref={ref}
+        type="checkbox"
+        // @ts-expect-error -- input switch が表示されるか確認したい
+        switch
+        className={css({ width: '[0]', height: '[0]' })}
+      />
+    </VibrationContext.Provider>
+  );
+};
+
+export const useVibration = () => {
+  const context = use(VibrationContext);
+  if (!context) {
+    throw new Error('useVibration must be used within a VibrationProvider');
+  }
+  return context;
+};
