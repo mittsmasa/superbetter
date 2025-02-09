70b89686de6507c0fb8592dd1293ddfe6e90ce3c
mittsmasa <light62motion@gmail.com>
2025-02-09 19:54:57 +0900

    fix: modify error fallback

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,42 @@
 'use client';
+
+import Cat from '@/app/icon.png';
+import { Button } from '@/components/button';
+import { useIsClient } from '@/hooks/check/client';
+import { css } from '@/styled-system/css';
+import Image from 'next/image';
+
 const ErrorFallback = () => {
-  return 'Error!!';
+  const isClient = useIsClient();
+  if (!isClient) {
+    return null;
+  }
+  return (
+    <div
+      className={css({
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '[100%]',
+        justifyContent: 'center',
+        gap: '16px',
+        padding: '16px',
+      })}
+    >
+      <p>しっぱいしちゃった...</p>
+      <p>もういっかいあくせすしてみて！</p>
+      <Image
+        src={Cat}
+        width={100}
+        height={100}
+        alt=""
+        className={css({ borderRadius: '50%' })}
+      />
+      <Button onClick={() => window.location.reload()} variant="secondary">
+        こうしん！
+      </Button>
+    </div>
+  );
 };
 
 export default ErrorFallback;
