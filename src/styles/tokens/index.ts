18bb245ec6cd01d7bb1092bf117b088a4b4c32a7
mittsmasa <light62motion@gmail.com>
2025-02-09 14:37:28 +0900

    feat: zIndex をトークンとして定義

--- src/styles/tokens/index.ts
+++ src/styles/tokens/index.ts
@@ -1,6 +1,8 @@
 import { defineTokens } from '@pandacss/dev';
 import { colorTokens } from './colors';
+import { zIndexTokens } from './z-index';
 
 export const tokens = defineTokens({
   colors: colorTokens,
+  zIndex: zIndexTokens,
 });
