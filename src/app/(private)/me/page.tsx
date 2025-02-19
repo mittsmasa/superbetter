1522fcc29297e3037989ede896b08c169e9ac278
mittsmasa <light62motion@gmail.com>
2025-02-19 17:50:48 +0900

    feat: マイページの操作を dropdown にしまう

--- src/app/(private)/me/page.tsx
+++ src/app/(private)/me/page.tsx
@@ -1,43 +1,26 @@
-import { signOut } from '@/auth';
-import { Button } from '@/components/button';
 import { css } from '@/styled-system/css';
+import { ConfigButton } from './_components/config-button';
 
-const Page = async (props: {
-  params: Promise<{ id: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) => {
-  const params = await props.params;
-  const searchParams = await props.searchParams;
+const Page = async () => {
   return (
     <main
       className={css({
         display: 'flex',
         flexDirection: 'column',
-        textStyle: 'Body.primary',
-        height: '[100%]',
       })}
     >
       <div
         className={css({
-          alignItems: 'center',
+          backgroundColor: 'black',
           display: 'flex',
-          flex: '1',
-          flexDirection: 'column',
-          gap: '24px',
-          justifyContent: 'center',
+          justifyContent: 'space-between',
+          padding: '8px',
+          position: 'sticky',
+          top: 0,
         })}
       >
-        <h1 className={css({ textStyle: 'Heading.primary' })}>
-          とりいそぎログアウトできるようにしたよ ↓
-        </h1>
-        <form
-          action={async () => {
-            'use server';
-            await signOut({ redirectTo: '/login' });
-          }}
-        >
-          <Button type="submit">ログアウト</Button>
-        </form>
+        <h1 className={css({ textStyle: 'Heading.primary' })}>マイページ</h1>
+        <ConfigButton />
       </div>
     </main>
   );
