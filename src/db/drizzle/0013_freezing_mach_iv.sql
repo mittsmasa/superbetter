8b1732fbcf7f489822ea5afd5e45acdc5da0b404
mittsmasa <light62motion@gmail.com>
2025-03-08 11:32:16 +0900

    fix: migration file が重複していたので手動で統合する

--- src/db/drizzle/0013_freezing_mach_iv.sql
+++ src/db/drizzle/0013_freezing_mach_iv.sql
@@ -1,3 +1,13 @@
+CREATE TABLE `villainHistory` (
+	`id` varchar(255) NOT NULL,
+	`villainId` varchar(255) NOT NULL,
+	`createdAt` datetime(3) NOT NULL,
+	`updatedAt` datetime(3) NOT NULL,
+	CONSTRAINT `villainHistory_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+ALTER TABLE `villainHistory` ADD CONSTRAINT `villainHistory_villainId_quest_id_fk` FOREIGN KEY (`villainId`) REFERENCES `quest`(`id`) ON DELETE cascade ON UPDATE no action;
+--> statement-breakpoint
 ALTER TABLE `villainHistory` DROP FOREIGN KEY `villainHistory_villainId_quest_id_fk`;
 --> statement-breakpoint
 ALTER TABLE `villainHistory` ADD CONSTRAINT `villainHistory_villainId_villain_id_fk` FOREIGN KEY (`villainId`) REFERENCES `villain`(`id`) ON DELETE cascade ON UPDATE no action;
