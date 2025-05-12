dca116fe73a3a5bc95fda44d3514939bebc33329
mittsmasa <light62motion@gmail.com>
2025-05-12 21:16:54 +0900

    feat: IconButton にストーリーをつける

---
+++ src/components/icon-button/with-label.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { AbTesting } from '@/assets/icons';
+import { IconButtonWithLabel } from './with-label';
+
+const meta = {
+  component: IconButtonWithLabel,
+  args: {
+    onClick: fn(),
+    children: <AbTesting />,
+    label: 'アイコンボタン',
+  },
+} satisfies Meta<typeof IconButtonWithLabel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {} satisfies Story;
