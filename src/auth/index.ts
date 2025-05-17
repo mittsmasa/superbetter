8f3bebc82e48c255f9c5e1a986433267f1a4b6cc
mittsmasa <light62motion@gmail.com>
2025-05-17 12:45:15 +0900

    chore: resend の provider を追加

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -4,6 +4,7 @@ import type { NextAuthConfig } from 'next-auth';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import Google from 'next-auth/providers/google';
+import Resend from 'next-auth/providers/resend';
 import { createInitialEntity } from '@/app/(private)/_server-only/create-initial-entity';
 import { users } from '@/db/schema/auth';
 import { db } from '../db/client';
@@ -18,6 +19,7 @@ const config = {
     },
   },
   providers: [
+    Resend,
     Google,
     Credentials({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
