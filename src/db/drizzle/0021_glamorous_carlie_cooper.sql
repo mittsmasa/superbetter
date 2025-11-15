6686c17a25b49c0280009dbe77e9434060328edb
mittsmasa <light62motion@gmail.com>
2025-11-15 21:01:55 +0900

    chore: TiDB の timestamp 型にあわせて変更

--- src/db/drizzle/0021_glamorous_carlie_cooper.sql
+++ src/db/drizzle/0021_glamorous_carlie_cooper.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `user` ADD `createdAt` timestamp(3) DEFAULT (now()) NOT NULL;--> statement-breakpoint
-ALTER TABLE `user` ADD `updatedAt` timestamp(3) DEFAULT (now()) NOT NULL;
+ALTER TABLE `user` ADD `createdAt` timestamp(3) DEFAULT CURRENT_TIMESTAMP(3) NOT NULL;--> statement-breakpoint
+ALTER TABLE `user` ADD `updatedAt` timestamp(3) DEFAULT CURRENT_TIMESTAMP(3) NOT NULL;
